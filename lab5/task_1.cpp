ded414233aea2bdfdb2e560369a6c689c117fff2
sugardaddy123 <werowkin@gmail.com>
2021-10-23 00:06:16 +0200

    Лабораторная 5: Задание 1

--- lab5/task_1.cpp
+++ lab5/task_1.cpp
@@ -1,19 +1,27 @@
-#include <stdio.h>
 #include "mpi.h"
 #include <iostream>
 
-using namespace std;
-
 int main(int argc, char* argv[])
 {
-    int rank, numtasks;
     MPI_Init(&argc, &argv);
 
+    int rank;
+    MPI_Status status;
+
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-    int buf = rank;
+    if (rank == 0)
+    {
+        char msg[11];
+        MPI_Recv(msg, 11, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &status);
 
+        std::cout << "Master get message: " << msg << '\n';
+    }
+    else if (rank == 1)
+    {
+        char msg[] = "Hello world";
+        MPI_Send(msg, 11, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
 
     MPI_Finalize();
 }
