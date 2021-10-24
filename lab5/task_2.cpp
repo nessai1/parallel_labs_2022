1e4d898522653be8867f58ec7bf5a09b3754236f
sugardaddy123 <werowkin@gmail.com>
2021-10-24 19:54:21 +0200

    Лабораторная работа 5: Задание 2

--- lab5/task_2.cpp
+++ lab5/task_2.cpp
@@ -5,10 +5,28 @@ int main(int argc, char* argv[])
 {
     MPI_Init(&argc, &argv);
 
-    int rank;
-    MPI_Status status;
+    int rank, size;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int message;
+    MPI_Status status;
+
+    if (rank == 0)
+    {
+        message = rank;
+        MPI_Send(&message, 1, MPI_INT, (rank+1) % size, 0, MPI_COMM_WORLD);
+    }
+
+    int sender = rank == 0 ? size - 1 : rank-1;
+    MPI_Recv(&message, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, &status);
+
+    std::cout << '[' << rank << ']' << ":receive message '" << message << "'\n";
+
+    message++;
+    MPI_Send(&message, 1, MPI_INT, (rank+1) % size, 0, MPI_COMM_WORLD);
+
+    MPI_Finalize();
     return 0;
 }
