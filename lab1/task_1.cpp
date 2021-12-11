5f9d94780d67a502a1d5d40991c7f17c4db3d98b
sugardaddy123 <werowkin@gmail.com>
2021-12-11 19:33:44 +0200

    Лабораторная работа №1: Пункты 1 и 2

--- lab1/task_1.cpp
+++ lab1/task_1.cpp
@@ -1,4 +1,15 @@
-//
-// Created by daniel on 11.12.2021.
-//
+#include <iostream>
+#include <omp.h>
 
+int main() {
+    int threads = 5;
+    omp_set_num_threads(threads); // Указали количество нитей = threads = 5
+
+    #pragma omp parallel
+    {
+        #pragma omp critical
+        {
+            std::cout << " I'm " << omp_get_thread_num() << " thread from " << omp_get_num_threads()  << " threads!\n";
+        }
+    };
+}
