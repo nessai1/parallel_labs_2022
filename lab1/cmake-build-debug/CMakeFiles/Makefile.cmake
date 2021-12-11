857b52092083a81965e93afe47ab43848a830c01
sugardaddy123 <werowkin@gmail.com>
2021-12-11 19:44:50 +0200

    Лабораторная работа №1: Пункты 1 и 2

--- lab1/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ lab1/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -46,5 +46,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/task_1_modified.dir/DependInfo.cmake"
   "CMakeFiles/task_1.dir/DependInfo.cmake"
   )
