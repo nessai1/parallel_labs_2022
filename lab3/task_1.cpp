5e9a6fb4a61cf842cb478ff754d69b15993a1059
sugardaddy123 <werowkin@gmail.com>
2021-12-12 21:32:02 +0200

    Лабораторная работа №3: Задание №1

--- lab3/task_1.cpp
+++ lab3/task_1.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <omp.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int k;
+    std::cout << "Enter number of threads (k): ";
+    std::cin >> k;
+
+    omp_set_num_threads(k);
+    #pragma omp parallel
+    {
+        #pragma omp sections
+        {
+            #pragma omp section
+            {
+                #pragma omp critical
+                std::cout << "[" << omp_get_thread_num() << "]: came in section "<< 1 << '\n';
+            }
+            #pragma omp section
+            {
+                #pragma omp critical
+                std::cout << "[" << omp_get_thread_num() << "]: came in section "<< 2 << '\n';
+            }
+            #pragma omp section
+            {
+                #pragma omp critical
+                std::cout << "[" << omp_get_thread_num() << "]: came in section "<< 3 << '\n';
+            }
+        }
+        #pragma omp critical
+        std::cout << "[" << omp_get_thread_num() << "]: parallel region\n";
+    };
     return 0;
 }
