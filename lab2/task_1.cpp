07c86981d4e2e04816d469cae961fac843e18d4d
sugardaddy123 <werowkin@gmail.com>
2021-12-12 02:55:24 +0200

    Лабораторная работа №2: Задание 1

---
+++ lab2/task_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <omp.h>
+
+int main()
+{
+    int k,N;
+
+    std::cout << "Enter number of threads (k): ";
+    std::cin >> k;
+    std::cout << "Enter N: ";
+    std::cin >> N;
+
+    omp_set_num_threads(k);
+
+    int sum = 0;
+    #pragma omp parallel
+    {
+        int chunk = 0;
+        #pragma omp for schedule(static) reduction(+:sum)
+        for (int i = 1; i <= N; i++)
+        {
+            chunk += i;
+        }
+
+        #pragma omp atomic
+        sum += chunk;
+
+        #pragma omp critical
+        std::cout << '[' << omp_get_thread_num() << "]: Sum = " << chunk << '\n';
+    }
+
+    std::cout << "Sum = " << sum;
+}
