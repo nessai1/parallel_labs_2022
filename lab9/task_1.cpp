71d7e34129cb021a9b19ce8a5d1e2114afb1bc0d
sugardaddy123 <werowkin@gmail.com>
2021-12-16 14:35:57 +0200

    Лабораторная 9: Задание №1

--- lab9/task_1.cpp
+++ lab9/task_1.cpp
@@ -1,14 +1,50 @@
 #include <iostream>
 #include <mpi.h>
+#include <algorithm>
 
 int main(int argc, char *argv[])
 {
     int size, rank;
+    MPI_Comm newcomm;
+    MPI_Group oldgroup, group;
     MPI_Init(&argc, &argv);
+
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_group(MPI_COMM_WORLD, &oldgroup);
 
-    std::cout << "I am " << rank << " process from " << size << " processes!!\n";
+    int count = (size/2) + (size%2);
+    int* evenThreads = new int[count];
+    for (int i = 0; i < size; i += 2)
+        evenThreads[i/2] = i;
+
+    MPI_Comm_group(MPI_COMM_WORLD, &oldgroup);
+    MPI_Group_incl(oldgroup, count, evenThreads, &group);
+    MPI_Comm_create(MPI_COMM_WORLD, group, &newcomm);
+
+    if (newcomm != MPI_COMM_NULL)
+    {
+        char msg[10];
+        std::fill(msg, msg + 10, 0);
+
+        int N;
+        int newrank, newsize;
+
+        MPI_Comm_size(newcomm, &newsize);
+        MPI_Comm_rank(newcomm, &newrank);
+
+        if (newrank == 0)
+        {
+            std::cout << "Enter N: ";
+            std::cin >> N;
+            std::cout << "Enter message: ";
+            std::cin >> msg;
+        }
+
+        MPI_Bcast(&N, 1, MPI_INT, 0, newcomm);
+        MPI_Bcast(&msg, N, MPI_CHAR, 0, newcomm);
+        std::cout << "MPI_COMM_WORLD: " << rank << " from " << size << ". Newcomm: " << newrank << " from " << newsize << ". Message = " << msg << '\n';
+    }
 
     MPI_Finalize();
     return 0;
