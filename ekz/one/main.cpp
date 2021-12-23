2b0fe778dfdcc611f1bd76813b4289b44462ac93
sugardaddy123 <werowkin@gmail.com>
2021-12-23 16:46:28 +0200

    Задание #1: сделал задание первое

--- ekz/one/main.cpp
+++ ekz/one/main.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
 #include <omp.h>
+#define THREADS 4
+
+double a1,a2,a3,a,b;
+int N;
+
+double f(double x)
+{
+    return (a1*x*x) + (a2*x) + a3;
+}
 
 int main() {
-    int threads = 5;
-    omp_set_num_threads(threads); // Указали количество нитей = threads = 5
+    std::cout << "Enter N: ";
+    std::cin >> N;
 
-#pragma omp parallel
+    std::cout << "Enter a1 a2 a3: ";
+    std::cin >> a1 >> a2 >> a3;
+
+    std::cout << "Enter a b: ";
+    std::cin >> a >> b;
+
+    omp_set_num_threads(THREADS);
+
+    double h = (a+b)/N;
+    double sum = 0;
+    double rs = 0;
+    #pragma omp parallel reduction(+ : rs) private(sum)
     {
-#pragma omp critical
+        #pragma omp  for
+        for (int i = 0; i < N; i++)
         {
-            std::cout << " I'm " << omp_get_thread_num() << " thread from " << omp_get_num_threads()  << " threads!\n";
+            double x = h * ((double)i + 0.5);
+            sum += f(x);
         }
-    };
+        rs = h * sum;
+    }
+
+    std::cout << "RESULT: " << rs;
 }
