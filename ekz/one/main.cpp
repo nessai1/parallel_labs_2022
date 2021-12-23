264a807bc97402e65c457b405da272a085be26da
sugardaddy123 <werowkin@gmail.com>
2021-12-23 13:54:33 +0200

    Задание #1: init

---
+++ ekz/one/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+    int threads = 5;
+    omp_set_num_threads(threads); // Указали количество нитей = threads = 5
+
+#pragma omp parallel
+    {
+#pragma omp critical
+        {
+            std::cout << " I'm " << omp_get_thread_num() << " thread from " << omp_get_num_threads()  << " threads!\n";
+        }
+    };
+}
